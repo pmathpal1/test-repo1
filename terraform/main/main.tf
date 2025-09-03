9b2d1cf4a8adfc7728e5125b82c4bd75b5487e12
Pankaj Mathpal <pankajmathpal@MAC-NB-253.local>
2025-09-03 12:27:30 +0530

    changes the rg and location

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -1,8 +1,14 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+
+}
+
 resource "azurerm_virtual_network" "vnet" {
-  count               = length(var.vnet)
+ count               = length(var.vnet)
   name                = var.vnet[count.index].name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.rg_name
   address_space       = [var.vnet[count.index].address_space]
 
 }
@@ -10,7 +16,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "subnets" {
   count                = length(var.subnet)
   name                 = var.subnet[count.index].subnet_name
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = var.rg_name
   virtual_network_name = var.subnet[count.index].name
   address_prefixes     = [var.subnet[count.index].address_prefix]
   depends_on = [
@@ -22,7 +28,3 @@ output "subnet_id" {
   value = azurerm_subnet.subnets.*.id
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-}
