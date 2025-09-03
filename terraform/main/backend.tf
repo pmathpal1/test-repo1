ddaacb50420a14bec6c82d2bf25c1b8f3e2c8ecb
Pankaj Mathpal <pankajmathpal@MAC-NB-253.local>
2025-09-03 12:02:40 +0530

    backend.tf added

--- terraform/main/backend.tf
+++ terraform/main/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "test-rg1"
-    storage_account_name = "pankajmathpal99001122"
+    storage_account_name = "pankajmathpal88774"
     container_name       = "mycon1212"
     key                  = "terraform.tfstate"
   }
